ad9638dd98875929bff3d3cabcba6b4a7ab3840e
Ashenfae <93448174+Ashenfae@users.noreply.github.com>
2025-12-13 23:01:27 +0100

    Update timing.h

--- boards/ergonautes/quacken_flex/timing.h
+++ boards/ergonautes/quacken_flex/timing.h
@@ -42,8 +42,7 @@
       compatible = "zmk,behavior-hold-tap";
       #binding-cells = <2>;
       tapping-term-ms = <TAPPING_TERM>;
-      flavor = "tap-preferred";
-	  require-prior-idle-ms = <200>;
+      flavor = "tap-unless-interrupted";
       bindings = <&kp>, <&kp>;
     };
     sc: space_cadet { // same as lt, but with hold-preferred
