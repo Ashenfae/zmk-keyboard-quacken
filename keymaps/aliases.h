76a21560e0778fe49b5377e497b4784e4537e475
Ashenfae <93448174+Ashenfae@users.noreply.github.com>
2026-02-20 21:04:12 +0100

    Revert "test numlock layer"
    This reverts commit 185937dead41da5cd0d2e33f895fe7639ca14684.

--- keymaps/aliases.h
+++ keymaps/aliases.h
@@ -28,27 +28,27 @@
 // Numbers
 
 #ifdef SHIFTED_NUMBERS
-  #define S_N0  LS(N0)
-  #define S_N1  LS(N1)
-  #define S_N2  LS(N2)
-  #define S_N3  LS(N3)
-  #define S_N4  LS(N4)
-  #define S_N5  LS(N5)
-  #define S_N6  LS(N6)
-  #define S_N7  LS(N7)
-  #define S_N8  LS(N8)
-  #define S_N9  LS(N9)
+  #define S_N0  &kp LS(N0)
+  #define S_N1  &kp LS(N1)
+  #define S_N2  &kp LS(N2)
+  #define S_N3  &kp LS(N3)
+  #define S_N4  &kp LS(N4)
+  #define S_N5  &kp LS(N5)
+  #define S_N6  &kp LS(N6)
+  #define S_N7  &kp LS(N7)
+  #define S_N8  &kp LS(N8)
+  #define S_N9  &kp LS(N9)
 #else
-  #define S_N0  N0
-  #define S_N1  N1
-  #define S_N2  N2
-  #define S_N3  N3
-  #define S_N4  N4
-  #define S_N5  N5
-  #define S_N6  N6
-  #define S_N7  N7
-  #define S_N8  N8
-  #define S_N9  N9
+  #define S_N0  &kp N0
+  #define S_N1  &kp N1
+  #define S_N2  &kp N2
+  #define S_N3  &kp N3
+  #define S_N4  &kp N4
+  #define S_N5  &kp N5
+  #define S_N6  &kp N6
+  #define S_N7  &kp N7
+  #define S_N8  &kp N8
+  #define S_N9  &kp N9
 #endif
 
 // Non-Alpha Actions
